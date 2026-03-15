9348c33c5ef88aed3597326e57104a9f5f63be02
Thiên Thành <48309788+thanh2210@users.noreply.github.com>
2026-03-15 15:17:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -270,9 +270,11 @@ else:
                             cell = ws_sanpham.find(str(sp['ma_sp']), in_column=1)
                             ws_sanpham.update_cell(cell.row, 4, ton_moi) # Cột Số lượng
                             ws_sanpham.update_cell(cell.row, 8, get_vn_time()) # Cột Thời gian
+
+                            log(loai.upper(), f"{qty} {sp['ma_sp']} - Lý do: {note}")
                             
                             # Tạo PDF
-                            pdf_bin = xuat_pdf_binary(user['ten_that'], sp['ma_sp'], sp['ten_sp'], sp['so_luong'], ton_moi)
+                            pdf_bin = xuat_pdf_binary(loai, user['ten_that'], sp['ma_sp'], sp['ten_sp'], qty, ton_moi, note)
                             st.success(f"Đã cập nhật tồn kho: {ton_moi}")
                             st.download_button("📥 TẢI PHIẾU PDF", data=pdf_bin, file_name=f"Phieu_{sp['ma_sp']}.pdf", mime="application/pdf")
                             if st.button("Làm mới bảng"): st.rerun()
